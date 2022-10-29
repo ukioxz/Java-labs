95b20801e379569ffbdb40f472db5fbe0caf0c02
ukioxz <ikatember@gmail.com>
2022-10-29 21:12:39 +0300

    Change string to stringbuilder

--- Lab3/Lab3.java
+++ Lab3/Lab3.java
@@ -1,15 +1,14 @@
-
+/* 0402
+ C3 = 0 StringBuilder
+ C17 = 11
+ */
 public class Lab3 {
 	public static void main(String[] args) {
-		String str = "Cheesecake is one of the most beloved desserts around "
-				+ "the globe. While many of us are acquainted with the fact that this creamy "
-				+ "dessert has its roots seated in New York, the fact is it actually dates back "
-				+ "much further.";
-		
-		String removeComma = str.replaceAll(",","");
-		String removePoint = removeComma.replace(".","");
+		String str = "Cheesecake is one of the most beloved "
+				+ "desserts around the globe";
+		StringBuilder sb = new StringBuilder(str);
 
-		String[] words = removePoint.split(" ");
+		String[] words = sb.toString().split(" ");
 		String consStr = "bcdfghjklmnprstvwxyzBCDFGHJKLMNPRSTVWXYZ";
 		for (String word : words) {
 			char result = word.charAt(0);
